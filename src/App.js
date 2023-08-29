ce39d0d1831c2ae6a648d811038a661cfa7bbf3d
Sergio Mogollon <113309955+i-am-sergio@users.noreply.github.com>
2023-08-29 20:51:21 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -14,18 +14,24 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                <div className='row mt-3'>
+                    {/* Add Budget component here under */}
+                    {/* // Budget component */}
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    {/* Add Remaining component here under */}
+                    {/* //Remaining component */}
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    {/* Add ExpenseTotal component here under */}
 
-                        {/* Add Remaining component here under */}        
+                    {/* Add ExpenseList component here under */}
 
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
+                    {/* Add ExpenseItem component here under */}
 
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
+                    {/* Add AllocationForm component here under */}
 
                 </div>
             </div>
